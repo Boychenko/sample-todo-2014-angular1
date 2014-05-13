0352d7b894a07e962e3824862102dd91c38d41db
Alexander Boychenko <boychenkoalexander@gmail.com>
2014-05-14 02:38:00 +0700

    redirect to login when auth error

--- Todo/Todo.Web/app/js/todo/config.authErrorHandler.js
+++ Todo/Todo.Web/app/js/todo/config.authErrorHandler.js
@@ -2,9 +2,12 @@
     'use strict';
 
     angular.module('todo').config(['$httpProvider', function($httpProvider) {
-        $httpProvider.interceptors.push(['$q', '$location', function ($q, $location) {
+        $httpProvider.interceptors.push(['$q', '$location', 'logger', function ($q, $location) {
             return {
                 'responseError': function (rejection) {
+                    if (rejection.status === 401 || rejection.status === 403 || rejection.status === 419 || rejection.status === 440) {
+                        $location.path('/login');
+                    }
                     return $q.reject(rejection);
                 }
             };
